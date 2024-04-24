b9272e147139bd5cf0c3a2f13528e64b45e2c3d2
dima.rogkov <dima.rogkov@cheitgroup.com>
2024-04-24 16:02:20 +0300

    add POST request

--- app/about/route.ts
+++ app/about/route.ts
@@ -3,3 +3,20 @@ import { comments } from '@/data/comments';
 export const GET = async () => {
     return Response.json(comments);
 };
+
+export const POST = async (request: Request) => {
+    const { text } = await request.json();
+    const newComment = {
+        id: comments.length + 1,
+        text,
+    };
+
+    comments.push(newComment);
+
+    return new Response(JSON.stringify(newComment), {
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        status: 201,
+    });
+};
