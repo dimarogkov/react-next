691bed802d513976fb89d84c39cf9ce0a0fb22ee
dima.rogkov <dima.rogkov@cheitgroup.com>
2024-04-25 15:23:25 +0300

    add PATCH request

--- app/about/[commentId]/route.ts
+++ app/about/[commentId]/route.ts
@@ -11,3 +11,11 @@ export const GET = (_request: Request, { params }: Props) => {
 
     return Response.json(currentComment);
 };
+
+export const PATCH = async (request: Request, { params }: Props) => {
+    const { text } = await request.json();
+    const index = comments.findIndex(({ id }) => id === +params.commentId);
+
+    comments[index].text = text;
+    return Response.json(comments[index]);
+};
